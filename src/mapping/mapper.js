f2c4e6831fa548a2ac669215031e217a61d4bbfa
Rob Dingwell <rob@robdingwell.org>
2018-12-31 10:52:50 -0500

    adding some addtional comments for clarity

--- src/mapping/mapper.js
+++ src/mapping/mapper.js
@@ -1,18 +1,18 @@
 const fhirpath = require('fhirpath');
-const _ = require('underscore');
+const _ = require("underscore");
 // embdedded function to inspect the results of fhir path calls to tell if something
 // returned an object or 'true'.  This is used to wrap the filter/ignore/exclude
 // functions to dtermin the truthyness of the fhir path calls
 let isTrue = (arg) => {
-  if (Array.isArray(arg) ){
+  if(Array.isArray(arg) ){
     return arg.find(i => isTrue(i));
-  } else if (typeof arg === 'object'){
+  }else if(typeof arg === "object"){
     return !_.isEmpty(arg);
-  } else if (typeof arg === 'string' && arg === 'false'){
+  }else if(typeof arg === "string" && arg ==="false"){
     return false;
   }
   return arg;
-};
+}
 
 // function to build the exec methods for FilterMappers.  The exec function modifies
 // the resource.  If the value is a string it will try to require the function else {
@@ -20,16 +20,16 @@ let isTrue = (arg) => {
 
 let buildProcessor = (arg) => {
   let processor = null;
-  switch (typeof arg) {
-    case 'string':
+  switch(typeof arg) {
+    case "string":
       processor = require(arg);
       break;
-    case 'function':
+    case "function":
       processor = arg;
       break;
   }
   return processor;
-};
+}
 
 // build a filter for use in the filter/ingnore/exclude operations
 // the filter may be a string or a function.  If it is a string it is treated as
@@ -37,22 +37,22 @@ let buildProcessor = (arg) => {
 // if it is a function then it will simply be returned.
 let buildFilter = (arg) => {
   // if string create a filter out of it
-  if (Array.isArray(arg)){
+  if(Array.isArray(arg)){
     let filters = arg.map( f => buildFilter(f));
-    return (resource) => {return filters.find( filter => isTrue(filter(resource)));};
+    return (resource) => {return filters.find( filter => isTrue(filter(resource)))}
   }
   let filter = null;
-  switch (typeof arg) {
-    case 'string': {
+  switch(typeof arg) {
+    case "string": {
       let path = fhirpath.compile(arg);
       filter = (resource) => isTrue(path(resource));
       break;}
-    case 'function':{
+    case "function":{
       filter = arg;
       break;}
   }
   return filter;
-};
+}
 
 // Build mappers from the arguments that are passed in.  If the args are null/undefined
 // return an empty array.
@@ -63,34 +63,34 @@ let buildFilter = (arg) => {
 // potential filters and or descriptions of the mapper and return an aggregate or filter
 // mapper depending on the rest of the attributes in the json object.
 let buildMappers = (args) =>{
-  if (!args) {return [];}
+  if(!args) {return []}
   // if the args are an array build an array of mappers to return
-  if (Array.isArray(args)){
+  if(Array.isArray(args)){
     return args.map(m => buildMappers(m));
   }
   // if the args are an object and it has a property called mappers
   // treat it like an aggregate mapper else like a filter mapper
-  if (args.mappers){
+  if(args.mappers){
     return new AggregateMapper(args);
-  } else if (args.exec){
-    return new FilterMapper(args);
-  } else { // treat this like an object mapping of  {"filter" : {mapping attributes}}
+  }else if(args.exec){
+    return new FilterMapper(args)
+  }else{ // treat this like an object mapping of  {"filter" : {mapping attributes}}
     let mappers = [];
-    for (var filter in args){
+    for(var filter in args){
       let mapper = args[filter];
-      if (typeof mapper === 'string'){
+      if(typeof mapper === 'string'){
         mappers.push(require(mapper));
-      } else if (typeof mapper === 'object' && !mapper.constructor.name === 'Object'){
+      }else if(typeof mapper === 'object' && !mapper.constructor.name === "Object"){
         mappers.push(mapper);
-      } else {
-        if (!mapper.filter){ mapper.filter = filter;}
-        if (!mapper.description){mapper.description = filter;}
+      }else {
+        if(!mapper.filter){ mapper.filter = filter};
+        if(!mapper.description){mapper.description = filter};
         mappers.push(buildMappers(mapper));
       }
     }
     return mappers;
   }
-};
+}
 
 // Class to contain other mappers in a heirachy.  In oder for the contained
 // mappers to be executed they the filter would have to match for the containing
@@ -106,32 +106,46 @@ class AggregateMapper {
     this.mappers = buildMappers(args.mappers);
   }
 
+  // if an ignore filter was provided execute it on the resource otherwise
+  // return false
   ignore(resource){
-    return this.ignoreFn ? this.ignoreFn(resource) : false;
+    return this.ignoreFn ? this.ignoreFn(resource) : false
   }
 
+  // if an exclude filter was provided execute it on the resource otherwise return false
   exclude(resource){
-    return this.excludeFn ? this.excludeFn(resource) : false;
+    return this.excludeFn ? this.excludeFn(resource) : false
   }
 
+  // if a default function was provided execute that function on the resource otherwise
+  // return the resource as is
   default(resource){
     return this.defaultFn ? this.defaultFn(resource) : resource;
   }
 
+  // if a filter was provided execute that on the resource otherwise
+  // return false
   filter(resource){
     return (this.filterFn) ? this.filterFn(resource) : false;
   }
 
+  // This method executes the aggregate filters.  There is a set order of operations
+  // for this method on a resource or set of resources passed in.
+  // ignore the resource if it returns true from the ignore function or does not pass the filter
+  // return null if the resource matches the exclude method
+  // if the resource matches a mapper that this aggregate mapper contains apply that mapper
+  // if the resource does not match a contained mapper run the default function on the resource
+  //
   execute(resource){
-    if (Array.isArray(resource)){
-      return resource.map( r => this.execute(r)).filter(n => n);
-    } else {
-      if (this.ignore(resource) || !this.filter(resource)){return resource;}
-      if (this.exclude(resource)){return null;}
+    if(Array.isArray(resource)){
+      return resource.map( r => this.execute(r)).filter(n => n)
+    }else{
+      if(this.ignore(resource) || !this.filter(resource)){return resource};
+      if(this.exclude(resource)){return null;};
       let mapper = this.mappers.find(map => map.filter(resource));
-      if (mapper){
+      if(mapper){
         return mapper.execute(resource);
-      } else {
+      }else{
         return this.default(resource);
       }
     }
@@ -149,13 +163,15 @@ class FilterMapper {
     this.execfn = buildProcessor(args.exec);
   }
 
+  // if a filter was provided execute that function on the resource otherwise
+  // return false
   filter(resource){
     return (this.filterFn) ? this.filterFn(resource) : false;
   }
 
   execute(resource){
-    if (Array.isArray(resource)){
-      return resource.map( r => this.execute(r)).filter(n => n);
+    if(Array.isArray(resource)){
+      return resource.map( r => this.execute(r)).filter(n => n)
     }
     return this.execfn(resource);
   }
@@ -165,4 +181,4 @@ module.exports = {
   AggregateMapper,
   FilterMapper,
   buildFilter
-};
+}
