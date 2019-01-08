f1f5503f5f92488b132a00f13118b32fbf787ae9
Rob Dingwell <rob@robdingwell.org>
2019-01-08 11:11:47 -0500

    adding a test for a bad token url

--- test/auth/controller.test.js
+++ test/auth/controller.test.js
@@ -13,8 +13,12 @@ describe('loading express', function() {
       .reply(200, metadata);
 
     nock('http://localhost:4001')
-      .post('/v/r2/auth/token')
+      .post('/v/r2/auth/token', body => body.grant_type=='authorization_code' && body.code=='code' && body.redirect_uri=='http://localhost:4000' && body.client_id=="CLIENT_ID" )
       .reply(200, token);
+
+    nock('http://localhost:4001')
+      .post('/v/r2/auth/token', body => body.grant_type!='authorization_code' ||  body.code!='code' || body.redirect_uri!='http://localhost:4000' || body.client_id!="CLIENT_ID" )
+      .reply(403, {});
   });
 
   afterEach(function() {
@@ -30,6 +34,20 @@ describe('loading express', function() {
   it('responds to token url', function testPath(done) {
     request(server)
       .post('/auth/token')
+      .send({grant_type:'authorization_code',
+                            redirect_uri: 'http://localhost:4000',
+                            code: 'code',
+                            client_id: 'CLIENT_ID'})
       .expect(200, done);
+  });
+
+  it('responds to bad token url call', function testPath(done) {
+    request(server)
+      .post('/auth/token')
+      .send({grant_type:'authorization_code',
+                            redirect_uri: 'http://localhost',
+                            code: 'code',
+                            client_id: 'CLIENT_ID'})
+      .expect(403, done);
   });
 });
