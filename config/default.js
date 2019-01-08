7de1cf5385c45a3e0d200958d3ea47f81e0098e6
Rob Dingwell <rob@robdingwell.org>
2019-01-08 11:54:32 -0500

    removing urls from config file

--- config/default.js
+++ config/default.js
@@ -37,10 +37,8 @@ const {
 
 
 let fhirClientConfig = {
-  baseUrl: 'https://fhir-ehr.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca',
+  baseUrl: 'http://localhost',
   debug: true
-  //baseUrl: 'http://moonshot-dev.mitre.org:4001/v/r2/'
-
 };
 
 /**c
