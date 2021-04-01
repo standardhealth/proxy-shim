958486011fb922d24aec27b0317ee56cbc1971c3
Benjamin Langley <blangley@mitre.org>
2021-04-01 15:34:30 -0400

    Fix Tests
    Remove test for authorize endpoint which has been removed
    Add jest.useFakeTimers()

--- test/auth/controller.test.js
+++ test/auth/controller.test.js
@@ -4,50 +4,61 @@ var request = require('supertest');
 var metadata = require('../resources/conformance');
 var token = require('../resources/token.good');
 
-describe('loading express', function() {
+describe('loading express', function () {
   let server;
+  jest.useFakeTimers();
   beforeEach(() => {
     server = require('../../src/index');
-    nock('http://localhost:4001')
-      .get('/v/r2/metadata')
-      .reply(200, metadata);
+    nock('http://localhost:4001').get('/v/r2/metadata').reply(200, metadata);
 
     nock('http://localhost:4001')
-      .post('/v/r2/auth/token', body => body.grant_type=='authorization_code' && body.code=='code' && body.redirect_uri=='http://localhost:4000' && body.client_id=="CLIENT_ID" )
+      .post(
+        '/v/r2/auth/token',
+        (body) =>
+          body.grant_type == 'authorization_code' &&
+          body.code == 'code' &&
+          body.redirect_uri == 'http://localhost:4000' &&
+          body.client_id == 'CLIENT_ID'
+      )
       .reply(200, token);
 
     nock('http://localhost:4001')
-      .post('/v/r2/auth/token', body => body.grant_type!='authorization_code' ||  body.code!='code' || body.redirect_uri!='http://localhost:4000' || body.client_id!="CLIENT_ID" )
+      .post(
+        '/v/r2/auth/token',
+        (body) =>
+          body.grant_type != 'authorization_code' ||
+          body.code != 'code' ||
+          body.redirect_uri != 'http://localhost:4000' ||
+          body.client_id != 'CLIENT_ID'
+      )
       .reply(403, {});
   });
 
-  afterEach(function() {
+  afterEach(function () {
     server.close();
-  });
-
-  it('responds to authorization url', function testSlash(done) {
-    request(server)
-      .get('/auth/authorize')
-      .expect(301, done);
   });
 
   it('responds to token url', function testPath(done) {
     request(server)
       .post('/auth/token')
-      .send({grant_type:'authorization_code',
-                            redirect_uri: 'http://localhost:4000',
-                            code: 'code',
-                            client_id: 'CLIENT_ID'})
+      .send({
+        grant_type: 'authorization_code',
+        redirect_uri: 'http://localhost:4000',
+        code: 'code',
+        client_id: 'CLIENT_ID',
+      })
       .expect(200, done);
   });
 
   it('responds to bad token url call', function testPath(done) {
     request(server)
       .post('/auth/token')
-      .send({grant_type:'authorization_code',
-                            redirect_uri: 'http://localhost',
-                            code: 'code',
-                            client_id: 'CLIENT_ID'})
+      .send({
+        grant_type: 'authorization_code',
+        redirect_uri: 'http://localhost',
+        code: 'code',
+        client_id: 'CLIENT_ID',
+      })
       .expect(403, done);
   });
 });
