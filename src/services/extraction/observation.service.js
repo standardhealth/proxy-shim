fb2e6878b7765119d0d9d3741e573648b8390fed
Julia Afeltra <jafeltra@mitre.org>
2021-08-04 08:13:05 -0400

    Extract Observations through extraction service
    - Implement search method on base extraction service class
    - Add utility function for converting MEF's collection bundle to a searchset bundle with only the relevant resource types present
    - Define a configuration object for extracting observations
    - Add sample CSV file for patient MRNS (other csv files use a url)

--- src/services/extraction/observation.service.js
+++ src/services/extraction/observation.service.js
@@ -1,8 +1,28 @@
 const BaseExtractionService = require('./base.service');
 
+const observationConfig = {
+  patientIdCsvPath: './test/sample-extraction-data/patient-mrns.csv',
+  extractors: [
+    {
+      label: 'patient',
+      type: 'CSVPatientExtractor',
+      constructorArgs: {
+        url: 'https://raw.githubusercontent.com/mcode/mcode-extraction-framework/main/test/sample-client-data/patient-information.csv',
+      },
+    },
+    {
+      label: 'observation',
+      type: 'CSVObservationExtractor',
+      constructorArgs: {
+        url: 'https://raw.githubusercontent.com/mcode/mcode-extraction-framework/main/test/sample-client-data/observation-information.csv',
+      },
+    },
+  ],
+};
+
 class ObservationService extends BaseExtractionService {
   constructor() {
-    super('Observation');
+    super('Observation', observationConfig);
   }
 }
 
